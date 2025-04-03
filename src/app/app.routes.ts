46d5ae7675465b4686c0e84f89e9d3b743d49682
sebastbas2310 <165617545+sebastbas2310@users.noreply.github.com>
2025-04-03 08:03:18 -0500

    prueba de actualizacion en vivo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { href } from 'react-router-dom';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+
+    path : "/home",
+    href : "/about",
+
+}];
