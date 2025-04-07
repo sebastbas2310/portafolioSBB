59a8feaa818bab9558d91806502d00849fee319a
sebastbas2310 <165617545+sebastbas2310@users.noreply.github.com>
2025-04-07 14:13:32 -0500

    a

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
-import { href } from 'react-router-dom';
 
-export const routes: Routes = [{
-
-    path : "/home",
-    href : "/about",
-
-}];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./components/home/items.module').then(m => m.ItemsModule)
+  },
+  {
+    path: 'projects',
+    loadChildren: () => import('./components/projects/orders.module').then(m => m.OrdersModule)
+  }
+];
